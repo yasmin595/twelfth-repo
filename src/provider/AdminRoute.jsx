c4645e198c0c820ea062c274d7dd9391ec2d2ad1
yasmin595 <yasminsuborna8@gmail.com>
2025-07-16 16:18:59 +0600

    slove admin

--- src/provider/AdminRoute.jsx
+++ src/provider/AdminRoute.jsx
@@ -1,27 +1,25 @@
 // routes/AdminRoute.jsx
 import React from "react";
 import { Navigate, useLocation } from "react-router";
-// import useAuth from "../hooks/useAuth";
-// import useUserRole from "../hooks/useUserRole";
-import Loading from "../Pages/shared/Loading";
 import useAuth from "../hooks/useAuth";
 import useUserRole from "../hooks/useUserRole";
+import Loading from "../Pages/shared/Loading";
 
-const UserRoute = ({ children }) => {
+
+const AdminRoute = ({ children }) => {
   const { user, loading } = useAuth();
   const { role, isRoleLoading } = useUserRole();
   const location = useLocation();
 
   if (loading || isRoleLoading) {
-    return <Loading />;
+    return <Loading></Loading> ;
   }
 
-  if (user && (role === "user" || role === "participant")) {
+  if (user && (role === "admin" || role === "organizer")) {
     return children;
   }
 
   return <Navigate to="/" state={{ from: location }} replace />;
 };
 
-export default UserRoute;
-
+export default AdminRoute;
